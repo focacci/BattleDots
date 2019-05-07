25e89b5d9574130d90b54a7c982c4ebcb34f4529
focacci <michaelfocacci@gmail.com>
2019-05-07 19:20:50 -0400

    Updated socket client

--- BattleDots/src/battleDots/desktop/SocketClient.scala
+++ BattleDots/src/battleDots/desktop/SocketClient.scala
@@ -1,5 +1,23 @@
 package battleDots.desktop
 
+import akka.actor.Actor
+import akka.io.Tcp._
+
+
+class SocketClient extends Actor {
+
+
+  override def receive: Receive = {
+    case c: Connected =>
+  }
+
+
+
+}
+
+
+
+
 object SocketClient {
 
 }
